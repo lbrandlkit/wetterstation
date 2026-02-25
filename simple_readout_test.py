66b01efc928d4b0d704683acfad4cfcdcc2e018d
Luc Brandl <luc.brandl@student.kit.edu>
2026-02-25 16:38:58 +0100

    first sensor test

---
+++ simple_readout_test.py
@@ -0,0 +1,17 @@
+import time
+import board
+import busio
+import adafruit_bme280
+
+# Create I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create sensor object (address 0x76 since your scan showed 76)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
+
+while True:
+    print("Temperature: {:.2f} °C".format(bme280.temperature))
+    print("Pressure: {:.2f} hPa".format(bme280.pressure))
+    print("Humidity: {:.2f} %".format(bme280.humidity))
+    print("-" * 30)
+    time.sleep(2)
